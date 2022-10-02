8d6c8cdf8d3cb084c0f1ed6333ff89ad8a32242d
Carina Ogbat <carinawrightogbat@gmail.com>
2022-10-02 16:10:12 -0700

    added letter buttons and boxes

--- static/js/sharkwords.js
+++ static/js/sharkwords.js
@@ -16,19 +16,24 @@ const WORDS = [
 ];
 
 const numWrong = 0;
-
 // Loop over the chars in `word` and create divs.
 // The divs should be appended to the section with id="word-container".
 const createDivsForChars = (word) => {
-  // Replace this with your code
-};
+  const wordContainer = document.querySelector('#word-container');
+  for (const letter of word){
+    wordContainer.insertAdjacentHTML('beforeend', `<div class="letter-box ${letter}"></div>`);
 
+  }
+};
 // Loop over each letter in the alphabet and generate a button for each letter
 // The buttons should be appended to the section with id="letter-buttons"
 const generateLetterButtons = () => {
-  // Replace this with your code
+  const letterButton = document.querySelector('#letter-buttons');
+  for (const letter of ALPHABET){
+    letterButton.insertAdjacentHTML('beforeend', `<button>${letter}</button>`)
+  }
+  
 };
-
 // Set the `disabled` property of `buttonEl` to `true.
 //
 // `buttonEl` is an `HTMLElement` object.
@@ -36,15 +41,12 @@ const generateLetterButtons = () => {
 const disableLetterButton = (buttonEl) => {
   // Replace this with your code
 };
-
 // This is a helper function we will use in the future
 // It should return `true` if `letter` is in the word
 // For now, you should test it out to make sure it works
-
 const isLetterInWord = (letter) => {
   // Replace this with your code
 };
-
 // This is like if __name__ == '__main__' in Python
 // It will be called when the file is run (because
 // we call the function on line 66)
@@ -56,11 +58,14 @@ const isLetterInWord = (letter) => {
   const word = 'hello';
 
   // call the function that makes an empty line for each letter in the word
-  // Replace this line with the function call
+  createDivsForChars(word);
+
 
   // call the function that makes a button for each letter in the alphabet
-  // Replace this line with the function call
+  generateLetterButtons();
 
   // in the next lab, you will be adding functionality to handle when
   // someone clicks on a letter
 })();
+
+startGame()
